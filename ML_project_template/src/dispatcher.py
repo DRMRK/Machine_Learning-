7f98940f979fc091d09fdc9e51b29f41ac6d1611
Mukut R Kalita <mukutrkalita@gmail.com>
2020-10-21 22:40:13 -0700

    removed template folder

--- ML_project_template/src/dispatcher.py
+++
@@ -1,6 +0,0 @@
-from sklearn import ensemble
-
-MODELS = {
-    "randomforest": ensemble.RandomForestClassifier(n_estimators=100,n_jobs=-1,verbose=2),
-    "extratrees": ensemble.ExtraTreesClassifier(n_estimators=100,n_jobs=-1,verbose=2)
-}
