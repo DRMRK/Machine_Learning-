5b1543707d43221890a246ff17b470730c487f52
Mukut R Kalita <mukutrkalita@gmail.com>
2020-12-14 16:37:17 -0800

    added requirements.txt

--- Bus_bunching_Translink/install_stuff.sh
+++
@@ -1,7 +0,0 @@
-conda install -y  -c anaconda jupyter 
-conda install -y -c anaconda pandas
-conda install -y -c anaconda numpy
-conda install -y -c conda-forge matplotlib 
-conda install -y -c anaconda seaborn 
-conda install -y -c conda-forge xgboost 
-conda install -y -c anaconda scikit-learn
