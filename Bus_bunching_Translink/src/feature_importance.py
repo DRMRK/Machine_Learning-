9ab611e21303f60ade718575d948cf2e15fa1012
Mukut R Kalita <mukutrkalita@gmail.com>
2021-01-07 15:43:22 -0800

    master

--- Bus_bunching_Translink/src/feature_importance.py
+++
@@ -1,8 +0,0 @@
-# We use permutation importance for this
-from sklearn.inspection import permutation_importance
-
-
-def feature_importance(model, X, y):
-    result = permutation_importance(model, X, y, n_repeats=10, random_state=0)
-    sorted_idx = result.importances_mean.argsort()
-    return result, sorted_idx
